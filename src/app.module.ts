2aa61afa70a8f415bc3a844e95140677fd1a63d4
Yaroslav Surilov <ysurilov@lohika.com>
2022-07-10 14:50:22 +0300

    FEATURE,SERVER,INFRA: setup GraphQL server, add typings generator from SDL, refactor

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,30 @@
 import { join } from 'path';
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+
+const isDevelopMode = process.env.DEVELOP === 'true';
 
 @Module({
   imports: [
       ServeStaticModule.forRoot({
-          rootPath: join(__dirname, '..', 'client'),
+          rootPath: join(__dirname, '..', 'client')
       }),
+      GraphQLModule.forRoot<ApolloDriverConfig>({
+          driver: ApolloDriver,
+          typePaths: ['./**/*.graphql'],
+          definitions: {
+              path: join(process.cwd(), 'src/graphql.ts'),
+          },
+          debug: isDevelopMode,
+          playground: isDevelopMode
+      })
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService]
 })
 
 export class AppModule {}
