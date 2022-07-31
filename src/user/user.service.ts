8d3d3a61886843b8a9c66c0bacc36d662d5aca15
Yaroslav Surilov <ysurilov@lohika.com>
2022-07-31 15:26:48 +0300

    FEATURE,SERVER,INFRA: add makefile, crud operations for user service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    async createUser () {}
+
+    async getUser (id: number) {}
+
+    async updateUser (id: number) {}
+
+    async deleteUser (id: number) {}
+}
